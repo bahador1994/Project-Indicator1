02f7f8afd967458ba21afd3c38d78ca9aa2ebf46
bahador1994 <bahador.eskandiany@gmail.com>
2025-01-07 00:47:50 +0330

    requirements

---
+++ requirements.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Install required packages
+
+pip install  pip
+pip install pandas
+pip install matplotlib
+pip install seaborn
+pip install statsmodels
+pip install scikit-learn
+pip install numpy
+pip install kaggle
